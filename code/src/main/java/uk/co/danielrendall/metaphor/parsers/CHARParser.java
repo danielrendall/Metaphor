3132878df0bb7ab4b4bd3c2a44b0855392989307
Daniel Rendall <drendall@gmail.com>
2012-08-03 20:40:30 +0100

    CHAR parser may possibly work.

--- code/src/main/java/uk/co/danielrendall/metaphor/parsers/CHARParser.java
+++ code/src/main/java/uk/co/danielrendall/metaphor/parsers/CHARParser.java
@@ -1,11 +1,17 @@
 package uk.co.danielrendall.metaphor.parsers;
 
+import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.co.danielrendall.metaphor.ParseException;
 import uk.co.danielrendall.metaphor.Parser;
 import uk.co.danielrendall.metaphor.ParserRegistry;
+import uk.co.danielrendall.metaphor.Record;
 import uk.co.danielrendall.metaphor.records.CHAR;
+import uk.co.danielrendall.metaphor.records.EMBELL;
 
 import java.io.PushbackInputStream;
+import java.util.List;
 
 /**
  * @author Daniel Rendall
@@ -14,7 +20,27 @@ public class CHARParser extends Parser<CHAR> {
 
     @Override
     protected CHAR doParse(PushbackInputStream in) throws ParseException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        Record.Options options = readOptions(in);
+        Record.Nudge nudge = options.nudge() ? readNudge(in) : Record.NO_NUDGE;
+        int typeFace = readTwoByteSignedInt(in);
+        int charCode = -1;
+        if (!options.char_enc_no_mtcode()) {
+            charCode = readSimple16BitInteger(in);
+        } else {
+            if (options.char_enc_char_8()) {
+                charCode = readByte(in);
+            } else if (options.char_enc_char_16()) {
+                charCode = readSimple16BitInteger(in);
+            } else {
+                throw new ParseException("Unable to read char code");
+            }
+        }
+        List<Record> embellishments = Lists.newArrayList();
+        if (options.char_embell()) {
+            readRecordsToEnd(in, embellishments);
+        }
+
+        return new CHAR(options, nudge, typeFace, charCode, embellishments);
     }
 
     @Override
--- code/src/main/java/uk/co/danielrendall/metaphor/records/CHAR.java
+++ code/src/main/java/uk/co/danielrendall/metaphor/records/CHAR.java
@@ -1,9 +1,46 @@
 package uk.co.danielrendall.metaphor.records;
 
+import com.google.common.collect.ImmutableList;
 import uk.co.danielrendall.metaphor.Record;
+
+import java.util.List;
 
 /**
  * @author Daniel Rendall
  */
 public class CHAR extends Record {
+    private final Options options;
+    private final Nudge nudge;
+    private final int typeFace;
+    private final int charCode;
+    private final List<Record> embellishments;
+
+    public CHAR(Options options, Nudge nudge, int typeFace, int charCode, List<Record> embellishments) {
+        //To change body of created methods use File | Settings | File Templates.
+        this.options = options;
+        this.nudge = nudge;
+        this.typeFace = typeFace;
+        this.charCode = charCode;
+        this.embellishments = ImmutableList.copyOf(embellishments);
+    }
+
+    public Options getOptions() {
+        return options;
+    }
+
+    public Nudge getNudge() {
+        return nudge;
+    }
+
+    public int getTypeFace() {
+        return typeFace;
+    }
+
+    public int getCharCode() {
+        return charCode;
+    }
+
+    public List<Record> getEmbellishments() {
+        return embellishments;
+    }
 }
