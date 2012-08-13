1f31eac99b291c620b6a1792f5f28f473118432a
Daniel Rendall <drendall@gmail.com>
2012-08-13 08:37:39 +0100

    Implemented parsing of COLOR_DEF (untested).

--- code/src/main/java/uk/co/danielrendall/metaphor/parsers/COLOR_DEFParser.java
+++ code/src/main/java/uk/co/danielrendall/metaphor/parsers/COLOR_DEFParser.java
@@ -1,11 +1,14 @@
 package uk.co.danielrendall.metaphor.parsers;
 
+import com.google.common.collect.Lists;
 import uk.co.danielrendall.metaphor.ParseException;
 import uk.co.danielrendall.metaphor.Parser;
 import uk.co.danielrendall.metaphor.ParserRegistry;
+import uk.co.danielrendall.metaphor.Record;
 import uk.co.danielrendall.metaphor.records.COLOR_DEF;
 
 import java.io.PushbackInputStream;
+import java.util.List;
 
 /**
  * @author Daniel Rendall
@@ -14,7 +17,15 @@ public class COLOR_DEFParser extends Parser<COLOR_DEF> {
 
     @Override
     protected COLOR_DEF doParse(PushbackInputStream in) throws ParseException {
-        throw new UnsupportedOperationException("Implement me!");  //To change body of implemented methods use File | Settings | File Templates.
+        Record.Options options = readOptions(in);
+        // default is RGB (3 colours) unless CMYK
+        int listSize = options.color_cmyk() ? 4 : 3;
+        List<Integer> colorValues = Lists.newArrayListWithCapacity(listSize);
+        for (int i=0; i<listSize; i++) {
+            colorValues.set(i, readSimple16BitInteger(in));
+        }
+        String name = options.color_name() ? readNullTerminatedString(in) : "";
+        return new COLOR_DEF(options, colorValues, name);
     }
 
     @Override
