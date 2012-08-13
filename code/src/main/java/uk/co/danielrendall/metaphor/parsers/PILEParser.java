cdd01ac03a4d61f80c121510bd45f05fbecf5601
Daniel Rendall <drendall@gmail.com>
2012-08-13 08:26:00 +0100

    Implemented parsing of PILE (untested).

--- code/src/main/java/uk/co/danielrendall/metaphor/parsers/PILEParser.java
+++ code/src/main/java/uk/co/danielrendall/metaphor/parsers/PILEParser.java
@@ -1,11 +1,15 @@
 package uk.co.danielrendall.metaphor.parsers;
 
+import com.google.common.collect.Lists;
 import uk.co.danielrendall.metaphor.ParseException;
 import uk.co.danielrendall.metaphor.Parser;
 import uk.co.danielrendall.metaphor.ParserRegistry;
+import uk.co.danielrendall.metaphor.Record;
 import uk.co.danielrendall.metaphor.records.PILE;
+import uk.co.danielrendall.metaphor.records.RULER;
 
 import java.io.PushbackInputStream;
+import java.util.List;
 
 /**
  * @author Daniel Rendall
@@ -14,7 +18,15 @@ public class PILEParser extends Parser<PILE> {
 
     @Override
     protected PILE doParse(PushbackInputStream in) throws ParseException {
-        throw new UnsupportedOperationException("Implement me!");  //To change body of implemented methods use File | Settings | File Templates.
+        Record.Options options = readOptions(in);
+        Record.Nudge nudge = options.nudge() ? readNudge(in) : Record.NO_NUDGE;
+        int hAlign = readByte(in); // docs don't explicitly say this is a byte, but seems likely
+        int vAlign = readByte(in);
+        RULER ruler = options.lp_ruler() ? (RULER)ParserRegistry.get(ParserRegistry.RULER).parse(in) : RULER.NO_RULER;
+        List<Record> records = Lists.newArrayList();
+        // There should only be LINE records in this list - is it worth enforcing this somewhere?
+        readRecordsToEnd(in, records);
+        return new PILE(options, nudge, hAlign, vAlign, ruler, records);
     }
 
     @Override
